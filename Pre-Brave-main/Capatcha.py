61b607b603bae75cade3e32bc7caba8e3f31cdeb
GD-hash <87503264+GD-hash@users.noreply.github.com>
2022-08-27 19:11:12 -0700

    Update Capatcha.py

--- Pre-Brave-main/Capatcha.py
+++ Pre-Brave-main/Capatcha.py
@@ -71,11 +71,12 @@ def CHECKifworked():
         if verify:
             print("PRESSING VERIFY AND FINISHING UP")
             pyautogui.click(verify)
-            time.sleep(6)            
+            time.sleep(10)            
             skip = pyautogui.locateOnScreen("img/skip.png")
             if skip:
                 print("DIDNT WORK TRYING AGAIN")
                 CPATCHA()
+                break
             # CLICK LOGIN
             print("CLICKING LOGIN")
             pyautogui.click(535, 514)
@@ -91,8 +92,6 @@ def CHECKifworked():
             GIF()
         else:
             # CELEBRATE GIF
-            GIF()
-            time.sleep(1)
             break
             
 # MAIN CAPTHCA LOGIC
@@ -481,33 +480,6 @@ def CPATCHA():
             print("POINTING AI TO FIND ALL BEDROOM IMAGES")
             # CREATE LIST OF CROPPED IMAGES TO MAKE INTO VAR FOR PYAUTOGUI
             dir_path = 'runs/detect/exp/crops/bedroom/'
-            # Iterate directory
-            res = os.listdir(dir_path)
-            print(res)
-            for x in range(len(res)):
-                print(res[x])
-                # CLICK THE IMAGES FOUND IN FILE
-                var2 = dir_path + res[x]
-                var1 = pyautogui.locateOnScreen(var2)
-                pyautogui.moveTo(var1)
-                print("LOOKING FOR CROPPED IMAGE")
-                print(var1)
-                print("CLICKING IMAGE")
-                pyautogui.click()
-                time.sleep(1)
-            IFNEXT = pyautogui.locateOnScreen("img/next.png")
-            if IFNEXT:
-                pyautogui.click(IFNEXT)
-                CPATCHA()
-                CHECKifworked()
-            else:
-                CHECKifworked()
-            break
-        if lionmane in text:
-            
-            print("POINTING AI TO FIND ALL LION WITH MANE IMAGES")
-            # CREATE LIST OF CROPPED IMAGES TO MAKE INTO VAR FOR PYAUTOGUI
-            dir_path = 'runs/detect/exp/crops/lion with mane on neck/'
             # Iterate directory
             res = os.listdir(dir_path)
             print(res)
